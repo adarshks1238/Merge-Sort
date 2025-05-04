13495f89823ddf742c1aa0e8522e0cb8d306d639
Adarsh K Sundaresan <adarshksundaresan395@gmail.com>
2025-05-04 19:15:33 +0530

    f

--- merge.py
+++ merge.py
@@ -23,21 +23,4 @@ def merge(low, mid, high):
         j += 1
         i += 1
 
-    for k in range(low, high + 1):
-        a[k] = b[k]
 
-
-a = list(map(int, input("Enter numbers separated by space: ").split()))
-
-print("\nBefore merge:", a)
-
-def merge_sort(low, high):
-    if low < high:
-        mid = (low + high) // 2
-        merge_sort(low, mid)
-        merge_sort(mid + 1, high)
-        merge(low, mid, high)
-
-merge_sort(0, len(a) - 1)
-
-print("After merge: ", a)
